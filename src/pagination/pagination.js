5102aa8571ee82476855e44cd92c8f9e9f54d419
leprokuda <leprokuda@gmail.com>
2022-07-28 13:35:20 +0700

    Исходники без стилей bootstrap (отвалились при восстановлении)

---
+++ src/pagination/pagination.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import '../css/App.css'
+
+const Paginator = ({
+  pages,
+  currentPage,
+  onNextClick,
+  onPreviousClick,
+  disabledPreviousBtn,
+  disabledNextBtn,
+  activeCurrentPage,
+  currentPageNumber
+}) => {
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="pagination justify-content-center">
+        <li className={`page-item ${disabledPreviousBtn}`}>
+          <a className="page-link text-secondary" href={"#" + currentPageNumber} tabIndex="-1" onClick={() => {onPreviousClick()}}>Назад</a>
+        </li>
+        {
+          pages.map(p => {
+            return (
+              <li className={(currentPageNumber === p) ? `page-item ${activeCurrentPage}` : `page-item`} key={p}>
+                <a className="page-link text-secondary" href={"#" + currentPageNumber} onClick={() => {currentPage(p)}}>{p}</a>
+              </li>
+            )
+          })
+        }
+        <li className={`page-item ${disabledNextBtn}`}>
+          <a className="page-link text-secondary" href={"#" + currentPageNumber} onClick={() => {onNextClick()}}>Далее</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Paginator
